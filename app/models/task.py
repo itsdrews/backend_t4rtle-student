37e4ce430ee4e7fd43550d10fd9e44743ed9444a
EmanuelAmi-UEA <eadshe.snf24@uea.edu.br>
2025-08-27 01:21:01 -0400

    Add completed_at parameter to Task model constructor and define completed_at column

--- app/models/task.py
+++ app/models/task.py
@@ -4,7 +4,7 @@ from ..extensions import db
 
 
 class Task(BaseModel):
-    def __init__(self, name, list_id, status, priority, description=None, delivery_time=None):
+    def __init__(self, name, list_id, status, priority, description=None, delivery_time=None, completed_at=None):
         self.name = name
         self.list_id = list_id
         self.status = status
@@ -12,6 +12,8 @@ class Task(BaseModel):
         self.description = description
         if delivery_time:
             self.delivery_time = delivery_time
+        if completed_at:
+            self.completed_at = completed_at
     __tablename__ = 'tasks'
     
     id = db.Column(db.Integer, primary_key=True)
@@ -21,3 +23,4 @@ class Task(BaseModel):
     list_id = db.Column(db.Integer, db.ForeignKey('task_lists.id'), nullable=False)
     delivery_time = db.Column(db.TIMESTAMP(timezone=True),default=lambda:datetime.now(timezone.utc))
     priority = db.Column(db.Integer,nullable=False)
+    completed_at = db.Column(db.DateTime, nullable=True)
